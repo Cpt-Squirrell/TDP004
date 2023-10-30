e7f7ba6b5344fd3b6058661138c5684c44aff3f0
Philip Bromander <phibr608@student.liu.se>
2023-10-30 13:55:53 +0100

    Cleaned up repository for the real course material

--- README.md
+++
@@ -1,1 +0,0 @@
-Created a new repository for TDP004 as part of the final test in TDP001.
--- test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-def main():
-	print("Hello world!")
-
-main()
-#Detta är en kommentar som skapats med echo
