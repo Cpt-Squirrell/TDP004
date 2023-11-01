e23614c9f026f06a25b24b39a6dc838b430fbcc8
Cpt-Squirrell <stelland1234@gmail.com>
2023-11-01 13:55:22 +0100

    Finished Lab2

--- lab2.cpp
+++ lab2.cpp
@@ -1,46 +1,71 @@
 #include <iostream>
-#include <string>
 #include <iomanip>
-#include <type_traits>
 
-using std::string, std::cout, std::cin, std::endl, std::flush;
+using std::cout, std::cin, std::endl, std::flush;
 
-float getInput(string variableName, float minValue = -100.0f, float maxValue = 100000.00f)
+float calculateVAT(float originalPrice, float tax)
 {
-    float number{};
-    do {
-        cout << "Mata in " << variableName << ": ";
-        cin >> number;
-        if(number < maxValue)
-            cout << "FEL: "<< variableName <<" måste vara minst " << minValue;
-    } while (number >= maxValue || number <= minValue);
-    return number;
+    return originalPrice * tax;
 }
 
-void generateList(float firstPrice, float lastPrice, float stepLength, float momsPercentage)
+float calculatePriceVAT(float originalPrice, float tax)
 {
-    cout << "\tPris\tMoms\tPris med moms\n-----------------------------------" << endl;
-    
-    //Generate formated generated tax list
-    for(float price = firstPrice ;price <= lastPrice; price += stepLength)
-    {
-        cout << "\t" << price <<"\t" << price <<"\t" << price << endl;
-    }
+    return originalPrice + calculateVAT(originalPrice, tax);
 }
 
 int main(int argc, char* argv[])
 {
+    cout << std::setw(12) << std::setprecision(2) << std::fixed;
 
-
-    cout << "INMATNINGSDEL\n============" << endl;
-    float firstPrice{       getInput("första pris")};
-    float lastPrice{        getInput("sista pris")};
-    float stepLength{       getInput("steglängd")};
-    float momsPercentage{   getInput("momsprocent")};
+    cout << "INMATNINGSDEL\n"
+         << std::setfill('=') << '\t'
+         << std::setfill(' ') << endl;
     
-    cout << "MOMSTABELLEN\n============" << endl;
-    generateList(firstPrice, lastPrice, stepLength, momsPercentage);
-    cout << endl;
+    float firstPrice = -1.00f;
+    while (firstPrice < 0.00f) {
+        cout << "Mata in första pris: " << flush;
+        cin >> firstPrice;
+        if (firstPrice < 0.00f)
+            cout << "Första pris måste vara minst 0 kronor!" << endl;
+    }
+
+    float lastPrice = 100001.00f;
+    while (lastPrice > 100000.00f || lastPrice < firstPrice) {
+        cout << "Mata in sista pris: " << flush;
+        cin >> lastPrice;
+        if (lastPrice > 100000.00f || lastPrice < firstPrice)
+            cout << "Sista pris måste vara större än första pris och mindre än 100 000 kronor!" << endl;
+    }
+    
+    float stepLength = 0.00f;
+    while (stepLength < 0.01f) {
+        cout << "Mata in steglängd: " << flush;
+        cin >> stepLength;    
+        if (stepLength < 0.01f)
+            cout << "Steglängden kan inte vara kortare än 0.01!" << endl;
+    }
+
+    float tax = -1.00f;
+    while (tax < 0.00f) {
+        cout << "Mata in momsprocent: " << flush;
+        cin >> tax;
+        if (tax < 0.00f)
+            cout << "Momsprocenten måste vara minst 0% (detta är ingen välfärd)!" << endl;
+    }
+    tax *= 0.01; // Make tax into 0.0 - 1.0 interval
+    
+    cout << "MOMSTABELLEN\n"
+         << std::setfill('=') << '\t'
+         << std::setfill(' ') << std::right << endl;
+
+    cout << "\tPris\tMoms\tPris med moms\n"
+         << std::setfill('-') << "\t\t\t"
+         << std::setfill(' ') << endl;
+    
+    for (float price = firstPrice; price <= lastPrice; price += stepLength)
+    {
+        cout << '\t' << price << '\t' << calculateVAT(price, tax) << '\t' << calculatePriceVAT(price, tax) << endl;
+    }
     
     return 0;
 }
